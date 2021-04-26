cbf02945dd8790c19a3fc250539bee601fe5d299
CanvasDEV-br <75525719+CanvasDEV-br@users.noreply.github.com>
2021-04-26 12:30:40 -0300

    Room Script

--- Assets/Scripts/WorldGeneration/RoomGenerator.cs
+++ Assets/Scripts/WorldGeneration/RoomGenerator.cs
@@ -7,31 +7,13 @@ public class RoomGenerator : MonoBehaviour
     public GameObject initialRoom;
     public GameObject bossRoom;
 
-    [Header("Rooms Prefabs")]
-    public GameObject[] roomsAll;
-    public GameObject[] roomsU;
-    public GameObject[] roomsD;
-    public GameObject[] roomsR;
-    public GameObject[] roomsL;
-
-    public GameObject[,] matrix = new GameObject[0,0];
-
-    public int quantityRooms;
-    public int quantityAlternativeRooms;
+    public int maxQuantityRooms;
     public float roomWidth;
 
-    [Range((int)0, (int)100)]
-    public int chancesToNull = 20; //what's the chance to NO spawn?
-    [Range((int)0, (int)100)]
-    public int chancesToRight = 50; //what's the chance to spawn for right?
-    [Range((int)0, (int)100)]
-    public int bossSpawnInTopCenter = 50; //what's the chance to spawn the room of boss in top center in finish row?
-
-    //if the initial rooms is in scene before starts, roomsInScene = 1, if it is generate in start roomsInScene = 0
-    private int totalCenterRoomsInScene = 0;
-    private int x;
-    private int y;
     private GameObject currentRoom; //current Alternative room
+    private GameObject newRoom;
+    private Vector3 nextPos;
+    private int quantityRooms;
 
     void Start()
     {
@@ -40,125 +22,19 @@ public class RoomGenerator : MonoBehaviour
 
     void Generate()
     {
-        GameObject initialPoint = Instantiate(initialRoom, new Vector3(0, 0, 0), Quaternion.identity);
-        //matrix[x, y] = initialPoint;
-        totalCenterRoomsInScene++;
+        GameObject initialObj = Instantiate(initialRoom, Vector3.zero, Quaternion.identity);
+        newRoom = initialObj.GetComponent<RoomID>().GetNextRoom();
+        quantityRooms++;
 
-        if (initialPoint.GetComponent<RoomID>().roomType == RoomType.U)
+        for (int i = 0; i <= maxQuantityRooms; i++)
         {
-            currentRoom = Instantiate(roomsD[Random.Range(0, roomsD.Length)], new Vector3(0, initialPoint.transform.position.y + roomWidth * totalCenterRoomsInScene, 0), Quaternion.identity);
-            y++;
-            //matrix[x, y] = currentRoom;
-            totalCenterRoomsInScene++;
+            nextPos = new Vector3(initialObj.transform.position.x, initialObj.transform.position.y + roomWidth * quantityRooms);
+            currentRoom = Instantiate(newRoom, nextPos, Quaternion.identity);
+            newRoom = currentRoom.GetComponent<RoomID>().GetNextRoom();
+            quantityRooms++;
         }
 
-        for (int i = 0; i < quantityRooms; i++)
-        {
-            currentRoom = InstantiateNewRoom(currentRoom.GetComponent<RoomID>());
-           // matrix[x, y] = currentRoom;
-        }
-    }
-
-    GameObject InstantiateNewRoom(RoomID room)
-    {
-        GameObject newRoom = new GameObject();
-        Vector2 newpos = new Vector2();
-        switch (room.roomType)
-        {
-            case RoomType.U:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-
-            case RoomType.D:
-                newRoom = roomsU[Random.Range(0, roomsU.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y - roomWidth);
-                y--;
-                break;
-
-            case RoomType.R:
-                newRoom = roomsL[Random.Range(0, roomsL.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x + roomWidth, room.transform.position.y);
-                x++;
-                break;
-
-            case RoomType.L:
-                newRoom = roomsR[Random.Range(0, roomsR.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x - roomWidth, room.transform.position.y);
-                x--;
-                break;
-
-            case RoomType.RLD:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                x++;
-                break;
-
-            case RoomType.RLU:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-
-                break;
-            case RoomType.RUD:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-
-            case RoomType.LUD:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-
-            case RoomType.UR:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-            case RoomType.UL:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-
-            case RoomType.UD:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-
-            case RoomType.DR:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-            case RoomType.DL:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-
-            case RoomType.RL:
-                newRoom = roomsD[Random.Range(0, roomsD.Length)];
-                newpos = new Vector2(room.gameObject.transform.position.x, room.transform.position.y + roomWidth);
-                y++;
-
-                break;
-        }
-        
-        return Instantiate(newRoom, newpos, Quaternion.identity);
+        currentRoom = Instantiate(bossRoom, nextPos, Quaternion.identity);
     }
 
     /*
--- Assets/Scripts/WorldGeneration/RoomID.cs
+++ Assets/Scripts/WorldGeneration/RoomID.cs
@@ -2,17 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum RoomType
-{
-    ALL,
-    U, D, R, L,
-    RLD, RLU, RUD, LUD,
-    UR, UL, UD,
-    DR, DL,
-    RL,
-}
-
 public class RoomID : MonoBehaviour
 {
-    public RoomType roomType;
+    public int chestRoomPercent = 0;
+    public int quantityEnemies = 5;
+    public GameObject[] compatibleRooms;
+    public GameObject[] enemies;
+    public Transform[] spawnPoints;
+    public Transform[] chestSpawnpoints;
+    public List<Portal> portals;
+    public GameObject chest;
+    public bool isChestRoom;
+    public bool isInAttack;
+    public int currentQuantityEnemies;
+
+    private void Start()
+    {
+        SpawnEnemies();
+    }
+
+    private void Update()
+    {
+        if (!isChestRoom && quantityEnemies <= 0 && isInAttack)
+        {
+            isInAttack = false;
+
+            for (int i = 0; i < portals.Count; i++)
+            {
+                portals[i].gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void SpawnEnemies()
+    {
+        for(int i = 0; i < quantityEnemies; i++)
+        {
+            if (quantityEnemies > 0)
+            {
+                GameObject temp = Instantiate(enemies[Random.Range(0, enemies.Length)], spawnPoints[Random.Range(0, spawnPoints.Length
+                    )].position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isInAttack = true;
+
+            for(int i = 0; i < portals.Count; i++)
+            {
+                portals[i].gameObject.SetActive(false);
+            }
+
+            foreach (GameObject enemy in enemies)
+            {
+                enemy.GetComponent<MolotovNPC>().isReady = true;
+            }
+        }
+    }
+
+    public GameObject GetNextRoom()
+    {
+        int rand = Random.Range(0, compatibleRooms.Length);
+        print(compatibleRooms.Length);
+        return compatibleRooms[rand];
+    }
 }
